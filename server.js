8625b0cd117bd602175f713ecc0e911081151d97
RenanFigueiredoEdglobo <renan.oliveira@edglobo.com.br>
2022-09-26 20:36:32 -0300

    update server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use('/',express.static(resolve(__dirname,"./build")));
 app.use('/cadastrar-noticia',express.static(resolve(__dirname,"./build")));
 app.use('/cadastrar-autor',express.static(resolve(__dirname,"./build")));
+app.use('/autores',express.static(resolve(__dirname,"./build")));
+app.use('/noticias',express.static(resolve(__dirname,"./build")));
+app.use('/editar-autor/:id',express.static(resolve(__dirname,"./build")));
+app.use('/editar-noticia/:id',express.static(resolve(__dirname,"./build")));
 app.use('*',express.static(resolve(__dirname,"./build")));
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
