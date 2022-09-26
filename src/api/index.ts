58717d0cbde7bc6652a0e90c4e5a76e5799e02a2
RenanFigueiredoEdglobo <renan.oliveira@edglobo.com.br>
2022-09-26 20:57:15 -0300

    usando a api hospedada

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://backend-projeto-edglobo.herokuapp.com/"
 })
 api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export default api
